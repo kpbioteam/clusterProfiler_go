72278ceed025be81dae10eb0a3fc1056e9157ec2
Ubuntu <ubuntu@ip-172-31-23-81.ec2.internal>
2018-03-19 17:51:52 +0000

    add plot

--- clusterProfiler_go.R
+++ clusterProfiler_go.R
@@ -1,24 +1,23 @@
 require("clusterProfiler", quietly = TRUE)
 require("org.Hs.eg.db", quietly = TRUE)
+require("ggplot2", quietly = TRUE)
 
 args <- commandArgs(trailingOnly = TRUE)
 
 input1 = args[1]
-input2 = args[2]
-output1 = args[3]
-output2 = args[4]
+output1 = args[2]
+output2 = args[3]
 
-gene <- c(read.table(input1))
-ego <- enrichGO(gene          = gene$V1,
-                OrgDb         = org.Hs.eg.db, 
-                keyType = input2)
+gene <- read.table(input1)
+ego <- enrichGO(gene          = c(gene$V2),
+                keyType = "ENTREZID",
+                OrgDb         = org.Hs.eg.db,
+                ont           = "BP",
+                pAdjustMethod = "BH",
+                pvalueCutoff  = 0.01,
+                qvalueCutoff  = 0.05,
+                readable      = TRUE)                
 
 write.table(ego, output1)
-
-png(file = output2,
-    width = 1200,
-    height = 600)
-
 dotplot(ego)
-
-dev.off()
+ggsave(file = output2, device = "pdf")
