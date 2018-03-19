27aed685436a6aabaf94484ef2995eda68a47c18
Ubuntu <ubuntu@ip-172-31-23-81.ec2.internal>
2018-03-19 10:32:08 +0000

    png -Binary data detected, not displaying diff

--- clusterProfiler_go.R
+++ clusterProfiler_go.R
@@ -5,11 +5,20 @@ args <- commandArgs(trailingOnly = TRUE)
 
 input1 = args[1]
 input2 = args[2]
-output = args[3]
+output1 = args[3]
+output2 = args[4]
 
 gene <- c(read.table(input1))
 ego <- enrichGO(gene          = gene$V1,
                 OrgDb         = org.Hs.eg.db, 
                 keyType = input2)
 
-write.table(ego, output)
+write.table(ego, output1)
+
+png(file = output2,
+    width = 1200,
+    height = 600)
+
+dotplot(ego)
+
+dev.off()
